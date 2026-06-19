158d4fa7012e5d5347b32df82052fa97054bc52d
Arif <phys.arif@gmail.com>
2026-06-20 02:58:47 +0600

    Add files via upload

--- scripts/generate_pages.js
+++ scripts/generate_pages.js
@@ -136,7 +136,6 @@ async function generateHomepage(bookList) {
     return `<a href="books/${book.slug}.html" class="bc-card">
       <div class="bc-img-wrap">
         <img src="${book.cover}" alt="${book.title}" class="bc-img" loading="lazy">
-        <span class="bc-num">${cardIndex}</span>
       </div>
       <span class="bc-meta">${book.title}</span>
       <span class="bc-author">${book.author_name}</span>
