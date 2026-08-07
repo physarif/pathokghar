8161a383d16e51e1e51e945e0c29d7253a94b787
Arif <phys.arif@gmail.com>
2026-08-08 01:41:26 +0600

    Update generate_pages.js

--- scripts/generate_pages.js
+++ scripts/generate_pages.js
@@ -247,7 +247,7 @@ async function generateBookPages() {
       book_read_href: book.zip_url ? `/read/${book.slug}.html` : 'javascript:void(0)',
       book_read_disabled: book.zip_url ? '' : 'btn-unclick',
       book_read_aria_disabled: book.zip_url ? 'false' : 'true',
-      book_download_href: book.download_url ? `/download/${book.slug}.html` : 'javascript:void(0)',
+      book_download_href: `/download/${book.slug}.html`,
       book_download_disabled: '',
       book_download_aria_disabled: 'false',
     });
